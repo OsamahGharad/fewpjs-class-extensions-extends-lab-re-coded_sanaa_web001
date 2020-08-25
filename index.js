dea8cb532bdbd82a53e505fadb5f4e8f927b610e
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-25 21:54:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+class Poligon {
+  constructor(array) {
+    this.array=array;
+  }
+  get countSides(){
+    return this.array.count;
+  }
+  get perimeter(){
+    return array.reduce((sum,side) => sum +side,0);
+  }
+}//end class Poligon
+
+class Triangle extends{
+
+  get isValid(){
+    if(array.indexOf(0)+array.indexOf(1) > array.indexOf(3)){
+      return true;
+    }
+    return false;
+  }
+}
+//end class triangle
+
+class square extends Poligon {
+  // get isValid(){
+  // 
+  // }
+  get area(){
+    return area= this.array.reduce(sum,side)=>sum + side ,0);
+  }
+}
