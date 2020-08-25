a4ae209cab919bafad0e8a584ae71293be8aeb52
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-25 22:02:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ class Triangle extends Polygon{
 }
 //end class triangle
 
-class square extends Polygon {
+class Square extends Polygon {
   get isValid(){
     return (this.sides[0]===this.sides[2] && this.sides[1] ===this.sides[3] &&this.sides[0]===this.sides[1])
   }
